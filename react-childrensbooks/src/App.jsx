9e6a5d7ed5a84a4d0f17cfb0d68f72ac75fa54a0
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-02-08 22:06:12 +0700

    Complete

--- react-childrensbooks/src/App.jsx
+++ react-childrensbooks/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import Allbooks from "./components/AllBooks/Allbooks";
 import "./App.css";
+import books from "./assets/childrensbooks.json";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  console.log(books);
 
   return (
     <div className="App">
